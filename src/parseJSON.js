2fddc5da32dd0a5663b3a3654b4378fdb72e01de
benninh <bensonninh@gmail.com>
2016-11-29 01:50:24 -0800

    Begin Parse - handles null and boolean JSONs

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,34 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+  // possible JSON values to read and parse
+  // null, number, boolean, string, array, object (nested values will be dealt with via recursion)
+  // check for json values
+
+  // initialize result holder of the parsed JSON
+  var parsedResult = 0;
+
+  // if "null" return null
+  if(json === "null"){
+  	parsedResult = null;
+  	//console.log("returned: " + parsedResult);
+  }
+
+  // if "boolean" return boolean
+  if(json === "true"){
+  	parsedResult = true;
+  	//console.log("returned: " + parsedResult);
+  }
+  else if(json === "false"){
+  	parsedResult = false;
+  	//console.log("returned: " + parsedResult);
+  }
+  
+  // if "number" return number
+
+  // if "string" return string
+
+
+  //return parsed JSON
+  return parsedResult;
 };
