e9adf096559d90a4c7d265eef5eaf8640efa1aa7
benninh <bensonninh@gmail.com>
2016-11-30 19:27:32 -0800

    reimplement getElementsByClassName to not declare a new subroutine. Previous implementation is still available, commented out at the bottom.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -12,69 +12,73 @@ https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
-  // BIG PICTURE ------ checking order
+// your code here
+  // checking order
   // bigNode(window.document) > childNodesOfDocument > childNodesOfCN > .... > until we can see a class list
   //                             |-------------recursion------------------|
   // when looking at class list, check for className
   /*
-	initialize resultsArray
-	define recursion function
-	  check if current node has a class list
-	    if yes, check classList for className --> push to results array
-	  check if current node has child nodes
-	    if yes, recurse through the childNode and go deeper in
-	call recursive function
-	return resultsArray
- */
-  // initialize array to hold results
+initialize resultsArray
+define recursion function
+  check if current node has a class list
+    if yes, check classList for className --> push to results array
+  check if current node has child nodes
+    if yes, recurse through the childNode and go deeper in
+  */
+var getElementsByClassName = function(className, thisOtherNode
+) {
   var resultsArray = [];
+  if(thisOtherNode === undefined){
+    thisOtherNode = window.document;
+  }
 
-  // write a function that takes a node as an argument to traverse the "tree"
-  var readNode = function(thisNode){
-    //console.log(thisNode.classList !== undefined ?  thisNode.classList.contains(className) : 'undefined')
-
-    /*
-		THIS SECTION HANDLES WHETHER OR NOT
-		A NODE HOLDS A LIST OF CLASSES
-    */
-    if (thisNode.classList !== undefined) {
-      if(thisNode.classList.contains(className)) {
-        resultsArray.push(thisNode)
-      }
+  if (thisOtherNode.classList !== undefined) {
+    if(thisOtherNode.classList.contains(className)) {
+      resultsArray.push(thisOtherNode);
     }
-    /*
-		THIS SECTION HANDLES WHETHER OR NOT
-		A NODE HAS CHILD NODES
-    */
-    if (thisNode.hasChildNodes()) {
-      // _.each(Array.prototype.slice.call(thisNode.childNodes), function(item) {
-      thisNode.childNodes.forEach(function(item) { //reading nodes has native forEach that is usable
-          readNode(item);
-      });
-    }
+  }
 
-    /*
-		THE TWO SECTIONS WORK TOGETHER, INDEPENDENTLY, TO 
-		1. CHECK FOR A CLASS LIST TO LOOK FOR A CLASS NAME
-		2. CHECK FOR CHILD NODES TO CONTINUE RECURSION
-    */
+  if(thisOtherNode.hasChildNodes()) {
+    // _.each(Array.prototype.slice.call(thisNode.childNodes), function(item) {
+    thisOtherNode.childNodes.forEach(function(item) {
+        //Array.prototype.concat(resultsArray, getElementsByClassName(className, item));
+              resultsArray = resultsArray.concat(getElementsByClassName(className, item));
+    });
+  }
+  else{
 
-    /*if (thisNode.hasChildNodes()) {
-      _.each(Array.prototype.slice.call(thisNode.childNodes), function(item) {
-          readNode(item);
-      });
-    }
-	// _.each([].slice.call) call is redundant because nodes have a native forEach to use*/
   };
-
-  readNode(window.document);
-  // readNode(document.body);
-
-
-  // return results array after we finish pushing to it
-  //console.log('resultsArray ', resultsArray)
+  //console.log(resultsArray);
   return resultsArray;
 };
+  
+
+//   // initialize array to hold results
+//   var resultsArray = [];
+
+//   // write a function that takes a node as an argument to traverse the "tree"
+//   var readNode = function(thisNode){
+//     //console.log(thisNode.classList !== undefined ?  thisNode.classList.contains(className) : 'undefined')
+
+//     if (thisNode.classList !== undefined) {
+//       if(thisNode.classList.contains(className)) {
+//         resultsArray.push(thisNode)
+//       }
+//     }
+
+//     if (thisNode.hasChildNodes()) {
+//       // _.each(Array.prototype.slice.call(thisNode.childNodes), function(item) {
+//       thisNode.childNodes.forEach(function(item) {
+//           readNode(item);
+//       });
+//     }
+//   };
+
+//   readNode(window.document);
+//   // readNode(document.body);
+
+
+//   // return results array after we finish pushing to it
+//   console.log('resultsArray ', resultsArray)
+//   return resultsArray;
+// };
